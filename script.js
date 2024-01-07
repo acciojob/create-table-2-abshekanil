4f358ec2e3842c3184ead0066c560a4f0b5bac36
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-07 16:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 function createTable() {
     //Write your code here
+	var rowCount = prompt("Input number of rows");
+
+	var colCount = prompt("Input number of columns");
+
+	var table = document.getElementById('myTable');
+
+	table.innerHTML = "";
+
+	for(var i = 0; i<rowCount; i++)
+		{
+			var row = table.insertRow(i);
+
+			for(var j=0; j<colCount; j++)
+				{
+					var cells = row.insertCell(j);
+
+					cell.textContent = "Row-"+ i + " Column-" + j;
+				}
+		}
   
 }
